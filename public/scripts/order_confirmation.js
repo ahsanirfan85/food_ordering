63ed0d7af4e9f0788480bd0991ce0d866e3a13bd
davincecode <hyperscript.ca@gmail.com>
2022-03-15 02:34:57 +0000

    Update recommendation on putting counter instead of random timer

--- public/scripts/order_confirmation.js
+++ public/scripts/order_confirmation.js
@@ -1,13 +1,13 @@
 const generateOrderNumber = () => {
-    return Math.random().toString(20).substr(2, 8);
-  };
+  return Math.random().toString(20).substr(2, 8);
+};
 const estimatedPrepTime = () => {
-    return Math.floor(Math.random() * 50) + 10;
-;}
+  return Math.floor(Math.random() * 50) + 10;
+};
 
 $(document).ready(function () {
-        const orderNumber = generateOrderNumber();
-        $('#orderNumber').text(orderNumber);
-        const estimatedTime = estimatedPrepTime();
-        $('#estimatedTime').text(`${estimatedTime} Minutes`);
+  const orderNumber = generateOrderNumber();
+  $("#orderNumber").text(orderNumber);
+  const estimatedTime = estimatedPrepTime();
+  $("#estimatedTime").text(`${estimatedTime} Minutes`);
 });
