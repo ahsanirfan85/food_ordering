fed57f6b5c1dab44b72f697e9153ec9ddd482018
Mustafa Paigeer <paigeer2012@gmail.com>
2022-03-16 21:28:27 +0000

    Add styles to orders.ejs

--- public/scripts/app2.js
+++ public/scripts/app2.js
@@ -6,13 +6,13 @@ $(document).ready(function() {
     $.ajax(`/api/orders/${id}`, { method: "GET" }).then(function (data) {
       for (const each of data.order) {
         console.log(each);
-        $('body').append(`Item: ${each.description}<br>`);
-        $('body').append(`Item Price: $ ${each.price / 100}<br>`);
-        $('body').append(`Quantity: $ ${each.quantity}<br>`);
-        $('body').append(`Cost: $ ${each.price / 100 * each.quantity}<br><br>`);
+        $('#orders').append(`Item: ${each.description}<br>`);
+        $('#orders').append(`Item Price: $ ${each.price / 100}<br>`);
+        $('#orders').append(`Quantity: $ ${each.quantity}<br>`);
+        $('#orders').append(`Cost: $ ${each.price / 100 * each.quantity}<br><br>`);
       }
 
-      $('body').append(`Order Status: ${data.order[0].status}<br>`);
+      $('#orders').append(`Order Status: ${data.order[0].status}<br>`);
 
     });
   };
